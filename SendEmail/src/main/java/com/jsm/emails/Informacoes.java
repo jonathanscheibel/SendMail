cb21d044bbd7bdbec30e42e7b3a168a6ba3ecdb6
jonathansmorais <jonathansmorais@gmail.com>
2016-06-23 00:53:03 -0300

    Ocultando senha

--- SendEmail/src/main/java/com/jsm/emails/Informacoes.java
+++ SendEmail/src/main/java/com/jsm/emails/Informacoes.java
@@ -2,5 +2,5 @@ package com.jsm.emails;
 
 public class Informacoes {
 	public static final String ARQUIVO_ANEXO 	= "C:\\Users\\Jonathan\\Desktop\\arquivo.txt";
-	public static final String SENHA_CONTA 		= "h4ck3r5ch31b37way109689";
+	public static final String SENHA_CONTA 		= "123456";
 }
