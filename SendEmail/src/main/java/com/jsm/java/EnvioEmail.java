2f7fd90c40d09c0f4ae645dc8bf9c3e3ef35be0e
jonathansmorais <jonathansmorais@gmail.com>
2016-06-22 23:20:46 -0300

    Tabulação

--- SendEmail/src/main/java/com/jsm/java/EnvioEmail.java
+++ SendEmail/src/main/java/com/jsm/java/EnvioEmail.java
@@ -5,12 +5,12 @@ import com.jsm.emails.EnvioJavaMail;
 
 /**
  * Fontes:
- * http://www.itcuties.com/java/javamail-send-email/
- * http://www.devmedia.com.br/utilizando-a-api-commons-email-para-enviar-e-mails/3306
- * http://stackoverflow.com/questions/1783710/sending-an-email-using-commons-email-to-gmail
- * http://www.devmedia.com.br/enviando-e-mail-com-anexo/1635
- * https://commons.apache.org/proper/commons-email/userguide.html
- * https://github.com/stephenc/simple-java-mail
+ * 	http://www.itcuties.com/java/javamail-send-email/
+ * 	http://www.devmedia.com.br/utilizando-a-api-commons-email-para-enviar-e-mails/3306
+ * 	http://stackoverflow.com/questions/1783710/sending-an-email-using-commons-email-to-gmail
+ * 	http://www.devmedia.com.br/enviando-e-mail-com-anexo/1635
+ * 	https://commons.apache.org/proper/commons-email/userguide.html
+ * 	https://github.com/stephenc/simple-java-mail
  */
 public class EnvioEmail {
 
