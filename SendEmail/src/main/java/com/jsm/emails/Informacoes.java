95948c542338d3db48e3d3c25fb6da2ec4636490
Jonathan <Jonathan@Jonathan-PC>
2016-06-23 14:18:46 -0300

    Alteração da Senha

--- SendEmail/src/main/java/com/jsm/emails/Informacoes.java
+++ SendEmail/src/main/java/com/jsm/emails/Informacoes.java
@@ -2,5 +2,5 @@ package com.jsm.emails;
 
 public class Informacoes {
 	public static final String ARQUIVO_ANEXO 	= "C:\\Users\\Jonathan\\Desktop\\arquivo.txt";
-	public static final String SENHA_CONTA 		= "123456";
+	public static final String SENHA_CONTA 		= "1234";
 }
